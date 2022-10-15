d82f20cc28d89906c7b99be2eef810d90e4293ac
JDave13 <111092059+JDave13@users.noreply.github.com>
2022-10-15 11:08:19 -0500

    Update Usuario.java

--- src/gestorAplicacion/usuario/Usuario.java
+++ src/gestorAplicacion/usuario/Usuario.java
@@ -6,43 +6,6 @@ import java.util.List;
 
 import baseDatos.Serializador;
 
-public class Cuenta implements Serializable{
-    
-    private int numeroCuenta;
-    private double saldo;
-    private String nombre;
-    
-    public String getNombre(){
-        return nombre;
-    }
-    
-    public Cuenta(int cuenta, double inicial) {
-        numeroCuenta = cuenta;
-        saldo = inicial;
-    } 
-
-    public void depositar(double cantidad) {
-        saldo = saldo + cantidad;
-    }
-
-    public void retirar(double cantidad) {
-        saldo = saldo - cantidad;
-    }
-
-    public double saldo() {
-        return saldo;
-    }  
-    
-}
-
-package gestorAplicacion.usuario;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
-import baseDatos.Serializador;
-
 public class Usuario implements Serializable{
 	
 	private static final long serialVersionUID = -64431385135968757L;
