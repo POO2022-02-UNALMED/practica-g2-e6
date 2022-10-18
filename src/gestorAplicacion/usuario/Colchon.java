1698fa84877940ff981e332bfc60b365ef18c7d1
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-10-18 09:01:56 -0500

    Update colchon.java

--- src/gestorAplicacion/usuario/Colchon.java
+++ src/gestorAplicacion/usuario/Colchon.java
@@ -8,12 +8,11 @@ public class Colchon extends Cuenta{
 	private static final long serialVersionUID = 514183129515203955L;
 	
 	private String nombre;
-	private Usuario usuario;
 	private String fechaRetiro;
 
 
-	public Colchon(double saldo, Divisa divisa, String Nombre, Usuario usuario, String fechaRetiro) {
-		super(divisa);
+	public Colchon(Usuario usuario, Divisa divisa, double saldo, String Nombre, String fechaRetiro) {
+		super(usuario,divisa);
 		this.depositar(saldo);
 		setNombre(nombre);
 		setUsuario(usuario);
@@ -26,14 +25,6 @@ public class Colchon extends Cuenta{
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
 	}
 
 	public String getFechaRetiro() {
