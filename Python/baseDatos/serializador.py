25807c4d223aee76bd36a4cf1dfd3ac517f1caa0
danielvillam <55595656+danielvillam@users.noreply.github.com>
2022-11-29 15:23:56 -0500

    ajuste

--- Python/baseDatos/serializador.py
+++ Python/baseDatos/serializador.py
@@ -1,10 +1,13 @@
 import pickle
+import pathlib
+import os
 
 class Serializador:
     def __init__(self, usuario):
         self._usuario = usuario
 
     def serializar(self):
-        picklefile = open("C:\\Users\\danii\\Documents\\GitHub\\practica-g2-e6\\Python\\baseDatos\\src\\tmp\\usuario.pkl", "wb")
+        path = os.path.join(pathlib.Path(__file__).parent.parent.parent.parent.absolute(),'practica-g2-e6\\Python\\baseDatos\\src\\tmp\\usuario.pkl')
+        picklefile = open(path, "wb")
         pickle.dump(self._usuario, picklefile)
         picklefile.close()
